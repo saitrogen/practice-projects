6c658c22119790e1cb7b61485eda0c14d1facef3
saitrogen <ashiquvava381@gmail.com>
2024-08-04 16:57:26 +0530

    adding and deleting added

--- DOM-js/assets/todolist.js
+++ DOM-js/assets/todolist.js
@@ -1,15 +1,47 @@
-const todoList=[]
+const todoList = ['eat', 'sleep', 'code'];
+
+displayList();
+function displayList() {
+  let todoListHTML = "";
+  for (let i = 0; i < todoList.length; i++) {
+    const task = todoList[i];
+
+    // const li = document.createElement('li');
+    // li.textContent = task;
+    // document.querySelector('ul').appendChild(li);
+
+    // document.querySelector('.js-task-area').innerHTML += `<p>${task}</p>`;
+
+    const html = `
+    
+    <p>
+      ${task}
+      <button onclick="
+      todoList.splice(${i}, 1);
+      displayList();
+      ">Delete</button>
+    </p>
+    
+    `;
+    todoListHTML += html; // todoListHTML = todoListHTML + html;
+    /* it does repeat the same thing but the innerHTML will remove 
+        the previous value inside the div fist then update the new value */
+    document.querySelector(".js-task-area").innerHTML = todoListHTML;
+    console.log(todoListHTML);
+  }
+}
 function handleKeydown() {
   console.log(event.key);
-  if (event.key === 'Enter') {
+  if (event.key === "Enter") {
     AddTask();
   }
 }
 function AddTask() {
-  const inputElement =document.querySelector('.js-input');
+  const inputElement = document.querySelector(".js-input");
   const inputs = inputElement.value;
   todoList.push(inputs);
   console.log(todoList);
   // to make inputbox empty after adding task
   inputElement.value = "";
+  displayList();
 }
